cae82d841bbf93f6c92aee0a4ff55e3735a97731
Andre Schumacher <andre.schumacher@stud.hs-flenburg.de>
2021-08-31 23:31:36 +0200

    Add loot to enemies

--- src/Enemy.elm
+++ src/Enemy.elm
@@ -11,6 +11,7 @@ module Enemy exposing
     )
 
 import Direction exposing (Direction(..))
+import Item exposing (Item)
 
 
 type EnemyStatus
@@ -30,26 +31,25 @@ type alias Enemy =
     , position : ( Int, Int )
     , lookDirection : Direction
     , enemyIMG : String
+    , loot : List Item
     , status : EnemyStatus
     }
 
 
 type EnemyType
-    = Troll
-    | Orc
-    | Warrior
-    | Cyclopes
+    = Cyclopes
     | Slime
     | Mole
 
 
-slime : Int -> ( Int, Int ) -> Enemy
-slime level position =
+slime : Int -> ( Int, Int ) -> List Item -> Enemy
+slime level position loot =
     { level = level
     , lifePoints = level * 10
     , attackDamage = level
     , strengthFactor = 1.0
     , experience = level * 100
+    , loot = loot
     , enemyType = Slime
     , prevPosition = position
     , position = position
@@ -59,13 +59,14 @@ slime level position =
     }
 
 
-mole : Int -> ( Int, Int ) -> Enemy
-mole level position =
+mole : Int -> ( Int, Int ) -> List Item -> Enemy
+mole level position loot =
     { level = level
     , lifePoints = level * 15
     , attackDamage = level
     , strengthFactor = 1.1
     , experience = level * 120
+    , loot = loot
     , enemyType = Mole
     , prevPosition = position
     , position = position
@@ -75,13 +76,14 @@ mole level position =
     }
 
 
-cyclopes : Int -> ( Int, Int ) -> Enemy
-cyclopes level position =
+cyclopes : Int -> ( Int, Int ) -> List Item -> Enemy
+cyclopes level position loot =
     { level = level
     , lifePoints = level * 20
     , attackDamage = level * 2
     , strengthFactor = 1.5
     , experience = level * 200
+    , loot = loot
     , enemyType = Cyclopes
     , prevPosition = position
     , position = position
@@ -91,19 +93,19 @@ cyclopes level position =
     }
 
 
-createEnemy : Int -> Maybe ( Int, Int ) -> EnemyType -> List Enemy
-createEnemy level pos enemyT =
+createEnemy : Int -> Maybe ( Int, Int ) -> EnemyType -> List Item -> List Enemy
+createEnemy level pos enemyT loot =
     case pos of
         Just position ->
             case enemyT of
                 Slime ->
-                    [ slime level position ]
+                    [ slime level position loot ]
 
                 Mole ->
-                    [ mole level position ]
+                    [ mole level position loot ]
 
                 _ ->
-                    [ cyclopes level position ]
+                    [ cyclopes level position loot ]
 
         Nothing ->
             []
