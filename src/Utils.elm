e9e29c32c710c43132758944d578167b906fb0cb
Andre Schumacher <andre.schumacher@stud.hs-flenburg.de>
2021-09-03 00:36:56 +0200

    Fix looting: stack items if already in inventory, enabling decrement on use

--- src/Utils.elm
+++ src/Utils.elm
@@ -1,14 +1,16 @@
 module Utils exposing
     ( experienceToLevelUp
     , gainExperience
+    , itemsToInventory
     , levelUp
-    , lootEnemy
     , oppositeDirection
     )
 
 import Direction exposing (Direction(..))
 import Enemy exposing (Enemy)
 import Environment
+import Item exposing (Item(..))
+import List.Extra
 import Player exposing (Player)
 
 
@@ -50,6 +52,36 @@ levelUp player =
     { player | experience = player.experience - experienceToLevelUp player.level, level = player.level + 1 }
 
 
-lootEnemy : Player -> Enemy -> Player
-lootEnemy player enemy =
-    { player | inventory = player.inventory ++ enemy.loot }
+itemsToInventory : List Item -> List Item -> List Item
+itemsToInventory inventory loot =
+    case loot of
+        item :: rest ->
+            if List.member item inventory then
+                itemsToInventory
+                    (List.Extra.updateIf
+                        (\invItem ->
+                            invItem == item
+                        )
+                        (\invItem -> stackItem invItem)
+                        inventory
+                    )
+                    rest
+
+            else
+                itemsToInventory (item :: inventory) rest
+
+        [] ->
+            inventory
+
+
+stackItem : Item -> Item
+stackItem item =
+    case item of
+        Foods f ->
+            Foods { f | stack = f.stack + 1 }
+
+        Potions p ->
+            Potions { p | stack = p.stack + 1 }
+
+        Weapons w ->
+            Weapons { w | stack = w.stack + 1 }
